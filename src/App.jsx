3f3c21f8f69c3c366f73c4785ec0fb8ef36d8420
Sarah Quinney <quinney.sarah@gmail.com>
2023-08-19 18:15:10 -0500

    practiced testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  
+//Test One
+// const App = () => <h1>Our First Test</h1>;
+
+// export default App;
+
+
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
 
   return (
-    <p>Test</p>
-  )
-}
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
-export default App
+export default App;
--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,23 +1,52 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
+//Test Two
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
 
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
+//Test One
+// describe("App component", () => {
+//   it("renders correct heading", () => {
+//     render(<App />);
+//     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+//   });
+// });
 
-    screen.debug();
+// describe('something truthy and falsy', () => {
+//   it('true to be true', () => {
+//     expect(true).toBe(true);
+//   });
 
-    // check if App components renders headline
-  });
-});
+//   it('false to be false', () => {
+//     expect(false).toBe(false);
+//   });
+// });
+
+// describe('App', () => {
+//   it('renders headline', () => {
+//     render(<App title="React" />);
+
+//     screen.debug();
+
+//     // check if App components renders headline
+//   });
+// });
